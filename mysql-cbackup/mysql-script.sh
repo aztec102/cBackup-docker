094cbc69695ceb0f01945ba8692adb6b07f602d9
chatlamin <viktorap@inbox.ru>
2019-09-27 17:32:56 +0300

    Delete mysql-script.sh

--- mysql-cbackup/mysql-script.sh
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE IF NOT EXISTS cbackup CHARSET utf8 COLLATE utf8_general_ci;
-CREATE USER IF NOT EXISTS 'cbackup'@'localhost' IDENTIFIED BY 'cbackup_password';
-GRANT USAGE ON *.* TO cbackup@localhost;
-GRANT ALL ON cbackup.* TO 'cbackup'@'127.0.0.1' IDENTIFIED BY 'cbackup_password';
-GRANT ALL ON cbackup.* TO 'cbackup'@'localhost' IDENTIFIED BY 'cbackup_password';
-GRANT ALL ON cbackup.* TO 'cbackup'@'%' IDENTIFIED BY 'cbackup_password';
